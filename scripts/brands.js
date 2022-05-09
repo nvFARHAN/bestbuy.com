8b6b8efcfd96a86b1ae13eb7ff2ab79475278ad3
nvFARHAN <nawaz.farhan26@gmail.com>
2022-05-09 10:40:31 +0530

    search functionality

--- scripts/brands.js
+++ scripts/brands.js
@@ -1,12 +1,19 @@
 import { menu } from "../components/menu.js";
 // console.log(menu())
 
+import navbar from "../components/navbar.js";
 
-import navbar from "../components/navbar.js"
-
-document.getElementById("navbar").innerHTML = navbar()
-
+document.getElementById("navbar").innerHTML = navbar();
 
 import footer from "../components/footer.js";
 
-document.getElementById("footer").innerHTML = footer()
+document.getElementById("footer").innerHTML = footer();
+
+let form = document.getElementById("form");
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  let key = form.search.value;
+  console.log(key);
+  localStorage.setItem("search", JSON.stringify(key));
+  window.location.href = "searchpro.html";
+});
